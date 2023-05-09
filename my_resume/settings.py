ea30d2cfcc0852981ba06fa3e3dbb4ac1668ad31
samukele-dev <samukele107@gmail.com>
2023-05-09 10:50:59 +0100

    aa

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'db.sqlite3')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
